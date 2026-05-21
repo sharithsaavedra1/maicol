4e9b40707139ce53b9df43dec51bd820bb480454
Emily Sharith Saavedra <mimisaavedra09@gmail.com>
2026-05-21 10:22:25 -0500

    Update script_v1.js

--- script_v1.js
+++ script_v1.js
@@ -1,19 +1,1 @@
-const envoltura = document.querySelector(".envoltura-sobre");
-const carta = document.querySelector(".carta");
 
-document.addEventListener("click", (e) => {
-    if (e.target.matches(".sobre") || 
-        e.target.matches(".solapa-derecha") ||
-        e.target.matches(".solapa-izquierda") ||
-        e.target.matches(".corazon")) {
-        envoltura.classList.toggle("abierto");
-        envoltura.classList.add("desactivar-sobre")
-
-     
-            setTimeout(() => {
-                carta.classList.remove("cerrando-carta");
-                carta.classList.remove("abierta")
-            }, 500);
-        }
-    }
-})
